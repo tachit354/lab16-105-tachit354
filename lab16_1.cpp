2f052ea078a7fe9b76316dd2654594260975badd
tachit354 <tachit_th@cmu.ac.th>
2026-02-05 18:59:24 +0700

    finish

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -21,7 +21,7 @@ int main(){
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
-	
+
 	cout << "---------------------------------------------\n";
 	
 	findRowSum(dPtr,sum1,N,M);
@@ -31,4 +31,39 @@ int main(){
 	
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
+}
+
+void showData(double *a, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        double data = ((double) rand() / (RAND_MAX));
+        *(a+i) = data;
+    }
+}
+
+void findRowSum(const double *a,double *sum1,int N,int M){
+    for(int i = 0; i < N; i++){
+        sum1[i] = 0;
+        for(int j = 0; j < M; j++){
+            sum1[i] += *(a+(M*i+j));
+        }
+    }
+}
+
+void findColSum(const double *a,double *sum2,int N,int M){
+    for(int i = 0; i < M; i++){
+        sum2[i] = 0;
+        for(int j = 0; j < N; j++){
+            if(N > 1) sum2[i] += *(a+(j*M+i));
+            else sum2[i] += *(a+i);
+        }
+    }
 }
